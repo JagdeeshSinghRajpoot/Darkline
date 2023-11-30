5ce8ce8150fecfd363a1fb58b67f46ce2d5f5f00
jagdeesh rajpoot <jagdeeshrajpoot9@gmail.com>
2023-11-30 18:56:03 +0530

    frist commit

---
+++ Infinity/src/main/java/com/darkline/Infinity/InfinityApplication.java
@@ -0,0 +1,13 @@
+package com.darkline.Infinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityApplication.class, args);
+	}
+
+}
